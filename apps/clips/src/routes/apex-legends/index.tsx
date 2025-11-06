ff545f40656f7f218fe2fc91ed68435e25915599
pluscosmic <harry@pluscosmic.dev>
2025-11-06 13:57:10 +0000

    Restyles

--- apps/clips/src/routes/apex-legends/index.tsx
+++ apps/clips/src/routes/apex-legends/index.tsx
@@ -1,23 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { ApexClips } from "../../components/Apex/ApexClips.tsx";
 
-type ApexLegendsSearch = {
-  page?: number
-}
-
 export const Route = createFileRoute('/apex-legends/')({
-  component: RouteComponent,
-  validateSearch: (search: Record<string, unknown>): ApexLegendsSearch => {
-    return {
-      page: Number(search?.page) || undefined,
-    }
-  },
+  component: RouteComponent
 })
 
 function RouteComponent() {
-  const search = Route.useSearch()
-
   return (
-    <ApexClips initialPage={search.page} />
+    <ApexClips/>
   )
 }
